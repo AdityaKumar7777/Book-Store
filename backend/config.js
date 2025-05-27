ba1072d809a7ec40e3cfbbc8d73448c9063facc5
aditya kumar <aditya.kr777@gmail.com>
2025-05-27 10:30:35 +0530

    git ig 3

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-export const DBPASSWORD = 9835912645;
-export const DBURL = `mongodb+srv://BookStore:${DBPASSWORD}@cluster0.wou37qw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0`;
